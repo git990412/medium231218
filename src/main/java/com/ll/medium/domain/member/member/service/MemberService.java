2ad7ebe24c766614a4d4c0fe1d088dd5f4c3ad12
git990412 <wjdwn282@gmail.com>
2023-12-19 09:57:33 +0900

    encode 추가

--- src/main/java/com/ll/medium/domain/member/member/service/MemberService.java
+++ src/main/java/com/ll/medium/domain/member/member/service/MemberService.java
@@ -2,6 +2,7 @@ package com.ll.medium.domain.member.member.service;
 
 import java.util.Optional;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,12 +17,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public void join(JoinForm joinForm) {
         memberRepository.save(Member.builder()
                 .username(joinForm.getUsername())
-                .password(joinForm.getPassword())
+                .password(passwordEncoder.encode(joinForm.getPassword()))
                 .email(joinForm.getEmail())
                 .build());
     }
