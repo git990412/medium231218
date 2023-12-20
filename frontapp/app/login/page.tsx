ed39bc1823db11e4a9a4786c43f10476194ef9f9
git990412 <wjdwn282@gmail.com>
2023-12-20 10:45:13 +0900

    로그인 페이지 수정

--- frontapp/app/login/page.tsx
+++ frontapp/app/login/page.tsx
@@ -1,7 +1,9 @@
 const Page = () => {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="flex justify-center">
+      <div className="w-3/4 max-w-sm flex flex-col items-center">
+        <h1 className="text-4xl font-bold mt-20">Login</h1>
+      </div>
     </div>
   );
 };
